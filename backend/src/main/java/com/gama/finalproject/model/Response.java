190f1e1e542deba0d8df327385b7d5dd112b86b9
chavesjoabe <chavesjoabe1@gmail.com>
2021-02-25 21:12:12 -0300

    ready to prod

--- backend/src/main/java/com/gama/finalproject/model/Response.java
+++ backend/src/main/java/com/gama/finalproject/model/Response.java
@@ -2,8 +2,10 @@ package com.gama.finalproject.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.context.annotation.Primary;
 import org.springframework.hateoas.RepresentationModel;
 
+@Primary
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class Response<T> extends RepresentationModel<Response<T>> {
