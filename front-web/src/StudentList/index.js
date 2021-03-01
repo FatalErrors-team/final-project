764c2431fc26ef06a883571d7d746913c0810d3b
João Victor <joaovicdsantos@gmail.com>
2021-03-01 00:00:31 -0300

    bicho eu ja perdi a criatividade

--- front-web/src/StudentList/index.js
+++ front-web/src/StudentList/index.js
@@ -5,17 +5,21 @@ import "./style.css";
 function StudentList({ estudantes }) {
   return (
     <div className="list-container">
+      {(estudantes === []) ||
+      <div className="card-main-description">
+        <h3>Não há alunos cadastrados.</h3>
+      </div> }
       {estudantes.map((estudante) => {
         return <Card nome={estudante.nome} curso={estudante.curso} conceito={estudante.conceito} />
       })}
+      {/* <Card nome="Nome" curso="Curso" conceito="Conceito" />
       <Card nome="Nome" curso="Curso" conceito="Conceito" />
       <Card nome="Nome" curso="Curso" conceito="Conceito" />
       <Card nome="Nome" curso="Curso" conceito="Conceito" />
       <Card nome="Nome" curso="Curso" conceito="Conceito" />
       <Card nome="Nome" curso="Curso" conceito="Conceito" />
       <Card nome="Nome" curso="Curso" conceito="Conceito" />
-      <Card nome="Nome" curso="Curso" conceito="Conceito" />
-      <Card nome="Nome" curso="Curso" conceito="Conceito" />
+      <Card nome="Nome" curso="Curso" conceito="Conceito" /> */}
     </div>
   );
 }
